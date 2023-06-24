80e660911ca6c6b1e29948248a33844cf6792297
woodpeckerduende <110867010+woodpeckerduende@users.noreply.github.com>
2023-06-24 20:32:39 -0300

    controlador produto

--- src/main/java/com/projetoAPI/loja/controllers/ProdutoController.java
+++ src/main/java/com/projetoAPI/loja/controllers/ProdutoController.java
@@ -1,4 +1,44 @@
 package com.projetoAPI.loja.controllers;
 
+import com.projetoAPI.loja.models.Produto;
+import com.projetoAPI.loja.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
+
+
 public class ProdutoController {
+    @Autowired
+    private  ProdutoRepository produtoRepository;
+    @GetMapping
+    public List<Produto> listandoProdutos() {
+        return  produtoRepository.findAll();
+    }
+    @PostMapping
+    public Produto adicionandoProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+    @PutMapping("/{id}")
+    public Produto atualizandoProduto(@PathVariable Long id, @RequestBody  Produto atualizandoProduto){
+        Optional<Produto> produtoOpcional = produtoRepository.findById(id);
+        if(produtoOpcional.isPresent()) {
+            Produto produtoAtualizado = produtoOpcional.get();
+            produtoAtualizado.setNome(atualizandoProduto.getNome());
+            produtoAtualizado.setPreco(atualizandoProduto.getPreco());
+            Produto produtoAdicionado = produtoRepository.save(produtoAtualizado);
+            return produtoAdicionado;
+        } else {
+            System.out.println("Erro ao encontrar o produto especificado");
+        }
+        return  atualizandoProduto;
+    }
+    @DeleteMapping("/{id}")
+    public void deletarProduto(@PathVariable Long id) {
+        produtoRepository.deleteById(id);
+    }
 }
